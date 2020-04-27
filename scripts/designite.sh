0c8e55d148e4bd9a215d33c3e63c0130400d1ff8
remdec <remydecocq@gmail.com>
2020-04-27 22:12:10 +0200

    sys 1 extension report

--- scripts/designite.sh
+++ scripts/designite.sh
@@ -2,5 +2,5 @@
 
 target=$1
 name="$(basename "$target")"
-java -jar resources/designite.jar -i $target -o outputs/${name}_DESIGNITE
+java -jar resources/designite.jar -i $target -o outputs/${name}_DESIGNITE_FINAL
 # -r JAiSLawGQ0wu5LTy
